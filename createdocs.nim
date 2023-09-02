00295e691a40521497fbb87a8c675e2f9894a912
olliNiinivaara <olli@verkkoyhteys.fi>
2023-09-02 19:40:40 +0300

    6.1.0

--- createdocs.nim
+++
@@ -1,9 +0,0 @@
-import guildenstern/[dispatcher, guildenserver, httpserver, streamingserver, websocketserver]
-import osproc
-discard execCmd("nim doc --project --index:on --outdir:docs createdocs.nim")
-
-# rm docs/*
-# cd guildenstern
-# nim buildIndex -o:../docs/theindex.html ../docs
-# nim doc --project --index:on --outdir:../docs guildenserver.nim
-
